ef36adf518cbe4cd56c921661aaa2aba87133131
mtrifonov <mtrifonov.design@gmail.com>
2025-11-24 12:33:49 +0100

    added images, basic structure in place

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,12 @@ function App() {
 
   return (
     <>
-      <img src={viteLogo} className="logo" alt="Vite logo" />
-      <h1>Hi, I'm Martin.</h1>
+    <div className='header'>
+      <img src={"/profilepicture.jpg"} className="logo" alt="Vite logo" />
+      <h2>Hi, I'm Martin! <br></br> 
+      I'm a Creative Technologist living in Berlin.</h2>
+    </div>
+
       <p>Welcome to my portfolio website!
       Here are links to my CV.
 
@@ -20,49 +24,49 @@ function App() {
       <ProjectPreview 
         title="Liquid Lissajous"
         description="2D WebGL render framework"
-        image="/lissajousextract.png"
+        image="/liquidlissajous_thumb.jpg"
       />
       <ProjectPreview 
         title="2D WebGL Render Framework"
         description="2D WebGL render framework"
-        image="/lissajousextract.png"
+        image="/pinsandcurvesengine_thumb.jpg"
       />
       <ProjectPreview 
         title="Pins and Curves"
         description="2D WebGL render framework"
-        image="/lissajousextract.png"
+        image="/pinsandcurves_thumb.jpg"
       />
       <h2>Math</h2>
       <ProjectPreview 
         title="Rotation + Translation = Rotation"
         description="2D WebGL render framework"
-        image="/lissajousextract.png"
+        image="/rotationtranslation_thumb.jpg"
       />
       <ProjectPreview 
         title="Prelude to Galois Theory"
         description="2D WebGL render framework"
-        image="/lissajousextract.png"
+        image="/preludetogaloistheory_thumb.jpg"
       />
       <ProjectPreview 
         title="Solutions to Artin Algebra"
         description="2D WebGL render framework"
-        image="/lissajousextract.png"
+        image="/algebra_thumb.jpg"
       />
       <h2>Art & Design</h2>
       <ProjectPreview 
         title="Rebranding 'Coffee Break'"
         description="2D WebGL render framework"
-        image="/lissajousextract.png"
+        image="/coffeebreak_thumb.jpg"
       />
       <ProjectPreview 
         title="Martin Trifonov, the bear?"
         description="2D WebGL render framework"
-        image="/lissajousextract.png"
+        image="/martintrifonovthebear_thumb.jpg"
       />
       <ProjectPreview 
         title="Making Sense of Branding"
         description="2D WebGL render framework"
-        image="/lissajousextract.png"
+        image="/makingsenseofbranding_thumb.jpg"
       />
     </>
   )
--- src/ProjectPreview/index.tsx
+++ src/ProjectPreview/index.tsx
@@ -6,7 +6,7 @@ export default function ProjectPreview(props: {
     image: string;
 }) {
     return <div className={styles.projectPreview}>
-        <img src="/lissajousextract.png" alt="" className={styles.projectImage} />
+        <img src={props.image} alt="" className={styles.projectImage} />
         <div className={styles.projectInfo}>
             <div className={styles.projectTitle}>{props.title}</div>
             <p className={styles.projectDescription}>{props.description}</p>
