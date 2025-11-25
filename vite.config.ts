bdfb62c12d1416a6ef4d9127b692c9366f323a26
mtrifonov <mtrifonov.design@gmail.com>
2025-11-25 16:43:16 +0100

    removed base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/martintrifonov-portfolio/',
+  //base: '/martintrifonov-portfolio/',
   plugins: [react()],
 })
