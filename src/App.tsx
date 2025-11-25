d44b7573e6d2c9f525bfd09ed7b1784c4c53b80c
mtrifonov <mtrifonov.design@gmail.com>
2025-11-25 14:12:33 +0100

    november update

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,96 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ProjectPreview from './ProjectPreview'
+import { GitHub, Linkedin, Youtube } from 'react-feather'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className='header'>
-      <img src={"/profilepicture.jpg"} className="logo" alt="Vite logo" />
-      <h2>Hi, I'm Martin! <br></br> 
-      I'm a Creative Technologist living in Berlin.</h2>
-    </div>
-
-      <p>Welcome to my portfolio website!
-      Here are links to my CV.
-
-      </p>
-      <div>Links</div>
-      <h2>Creative Tools</h2>
-      <ProjectPreview 
+      <div className='header'>
+        <img src={"/profilepicture.jpg"} className="logo" alt="Profile picture of Martin" />
+        <div style={{
+          fontSize: '1.5rem',
+        }}>
+          <div style={{
+            fontWeight: '600',
+          }}>
+            Martin Trifonov
+          </div>
+          <a href="mailto:mtrifonov.design@gmail.com">mtrifonov.design@gmail.com</a>
+                <div style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  gap: '0.5rem',
+                  marginTop: '0.2rem',
+                  alignItems: 'center',
+                }}>
+                  <a href="https://www.youtube.com/@martintrifonov" target='_blank'><Youtube /></a>
+                  <a href="https://www.linkedin.com/in/martin-trifonov-945581196/" target='_blank'><Linkedin /></a>
+                  <a href="https://github.com/mtrifonov-design" target='_blank'><GitHub /></a>
+                  <a href="https://mtrifonov.notion.site/mtrifonov/Martin-Trifonov-910d65b2c7054243a136ce461635c6d9" target='_blank'>CV</a>
+                </div>
+        </div>
+      </div>
+      <div style={{
+        height: '2rem',
+      }}></div>
+      {/* <h2>Creative Tools</h2> */}
+      <ProjectPreview
         title="Liquid Lissajous"
-        description="2D WebGL render framework"
+        description="Web app for creating looping generative gradient animations from Lissajous curves."
+        impact="500+ exported animations"
+        href="https://liquidlissajous.pinsandcurves.app"
         image="/liquidlissajous_thumb.jpg"
       />
-      <ProjectPreview 
-        title="2D WebGL Render Framework"
-        description="2D WebGL render framework"
+      <ProjectPreview
+        title="WebGL-based 2D Graphics Engine"
+        description="Lightweight render-graph-style engine for 2D GPU graphics."
         image="/pinsandcurvesengine_thumb.jpg"
+        href="https://github.com/mtrifonov-design/pinsandcurves-engine"
       />
-      <ProjectPreview 
+      <ProjectPreview
         title="Pins and Curves"
-        description="2D WebGL render framework"
+        description="A platform offering small generative web tools for designers."
         image="/pinsandcurves_thumb.jpg"
+        href="https://pinsandcurves.app"
       />
-      <h2>Math</h2>
-      <ProjectPreview 
+      <ProjectPreview
         title="Rotation + Translation = Rotation"
-        description="2D WebGL render framework"
+        description="Visual proof of a linear algebra theorem, animated with a custom engine."
+        impact="70k+ views; honorable mention in 3Blue1Brown's SoME3 contest"
         image="/rotationtranslation_thumb.jpg"
+        href="https://youtu.be/1EpQtVJb0OU"
       />
-      <ProjectPreview 
+      <ProjectPreview
         title="Prelude to Galois Theory"
-        description="2D WebGL render framework"
+        description="An introduction to symmetric polynomials and their role in Galois theory."
+        impact="50k+ views"
         image="/preludetogaloistheory_thumb.jpg"
+        href="https://youtu.be/3imeTgGBaLc"
       />
-      <ProjectPreview 
+      {/* <ProjectPreview
         title="Solutions to Artin Algebra"
         description="2D WebGL render framework"
         image="/algebra_thumb.jpg"
       />
-      <h2>Art & Design</h2>
-      <ProjectPreview 
+      <h2>Art & Design</h2> */}
+      {/* <ProjectPreview
         title="Rebranding 'Coffee Break'"
         description="2D WebGL render framework"
         image="/coffeebreak_thumb.jpg"
-      />
-      <ProjectPreview 
+      /> */}
+      {/* <ProjectPreview
         title="Martin Trifonov, the bear?"
         description="2D WebGL render framework"
         image="/martintrifonovthebear_thumb.jpg"
       />
-      <ProjectPreview 
+      <ProjectPreview
         title="Making Sense of Branding"
         description="2D WebGL render framework"
         image="/makingsenseofbranding_thumb.jpg"
-      />
+      /> */}
     </>
   )
 }
--- src/ProjectPreview/index.tsx
+++ src/ProjectPreview/index.tsx
@@ -4,13 +4,19 @@ export default function ProjectPreview(props: {
     title: string;
     description: string;
     image: string;
+    impact?: string;
+    href: string;
 }) {
-    return <div className={styles.projectPreview}>
+    return <a className={styles.projectPreview}
+        href={props.href}
+        target="_blank"
+    >
         <img src={props.image} alt="" className={styles.projectImage} />
         <div className={styles.projectInfo}>
             <div className={styles.projectTitle}>{props.title}</div>
             <p className={styles.projectDescription}>{props.description}</p>
+            {props.impact && <div className={styles.projectImpact}>{props.impact}</div>}
         </div>
 
-    </div>
+    </a>
 }
